66f0920596bcde7a3962892dde2e6df2dfba17c7
Eric Moynihan <git@moynihan.io>
2020-01-27 19:40:16 +0000

    Finished button 1

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,21 +9,26 @@ export interface ButtonConfig extends Base {
 export default function Button(props: ButtonConfig): JSX.Element {
   const [ripple, setRipple] = React.useState(style.ripple);
   const [coords, setCoords] = React.useState({ top: 0, left: 0 });
+  const ref = React.useRef<HTMLButtonElement>();
 
   const onClick = (e: React.MouseEvent) => {
-    let [x, y] = [e.pageX, e.pageY];
+    let [x, y] = [
+      e.pageX - ref?.current.offsetLeft,
+      e.pageY - ref?.current.offsetTop
+    ];
 
     setCoords({ top: y, left: x });
-    setRipple(`${style.ripple} ${style.rippleAnimation}`);
+    setRipple(`${style.rippleAnimation} ${style.ripple}`);
     setTimeout(() => setRipple(style.ripple), 500);
 
-    props?.onClick(e);
+    // Passthrough event to user defined onClick
+    if (props.onClick) props.onClick(e);
   };
 
   return (
-    <button onClick={onClick} className={style.root}>
-      <div className={style.button}>{props.children}</div>
+    <button ref={ref} onClick={onClick} className={style.root}>
       <div style={coords} className={ripple}></div>
+      <div className={style.button}>{props.children}</div>
     </button>
   );
 }
