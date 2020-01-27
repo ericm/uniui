74d5f3f7868a74045d52958f9ebc8336a3635b58
Eric Moynihan <git@moynihan.io>
2020-01-27 14:32:12 +0000

    Added root vars

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,9 +2,16 @@ import * as React from "react";
 import { Base } from "./base";
 import * as style from "./styles/Button.css";
 
-export interface ButtonConfig extends Base {}
-export default function(props: ButtonConfig): JSX.Element {
+export interface ButtonConfig extends Base {
+  onClick: React.EventHandler<any>;
+}
+
+export default function Button(props: ButtonConfig): JSX.Element {
   const [clicked, setClicked] = React.useState(false);
 
-  return <div className={style.root}>{props.children}</div>;
+  return (
+    <div onClick={props.onClick} className={style.root}>
+      <div className={style.button}>{props.children}</div>
+    </div>
+  );
 }
