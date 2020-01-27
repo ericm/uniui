ce19ba8b9999b48fe239ab4a0b56892e1c65ee46
Eric Moynihan <git@moynihan.io>
2020-01-27 15:55:53 +0000

    Set onclick

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,15 +3,26 @@ import { Base } from "./base";
 import * as style from "./styles/Button.css";
 
 export interface ButtonConfig extends Base {
-  onClick: React.EventHandler<any>;
+  onClick?: React.EventHandler<React.MouseEvent>;
 }
 
 export default function Button(props: ButtonConfig): JSX.Element {
-  const [clicked, setClicked] = React.useState(false);
+  const [ripple, setRipple] = React.useState(style.ripple);
+  const [coords, setCoords] = React.useState({top: 0, left: 0})
+
+  const onClick = (e: React.MouseEvent) => {
+    let [x, y] = [e.pageX, e.pageY];
+
+    setRipple(`${style.ripple} ${style.rippleAnimation}`);
+    setTimeout(() => setRipple(style.ripple), 500);
+
+    props?.onClick(e);
+  };
 
   return (
-    <button onClick={props.onClick} className={style.root}>
+    <button style={{top:}} onClick={props.onClick} className={style.root}>
       <div className={style.button}>{props.children}</div>
+      <div className={ripple}></div>
     </button>
   );
 }
