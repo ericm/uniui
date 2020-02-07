d5cf4d94807036c549341da968caea687adcd737
Eric Moynihan <git@moynihan.io>
2020-02-07 13:54:53 +0000

    Added subtitle to TextField

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -6,10 +6,16 @@ import * as style from "./styles/TextField.css";
 
 export interface InputConfig extends Base {
   value?: string;
+  subtitle?: string;
 }
-export default function Input(props: InputConfig) {
+export default function Input(props: InputConfig): JSX.Element {
   return (
     <div className={style.root}>
+      {(() =>
+        props.subtitle ? (
+          <span className={style.sub}>{props.subtitle}</span>
+        ) : null)()}
+      <span>{props.subtitle}</span>
       <input value={props.value} style={props.style}></input>
     </div>
   );
