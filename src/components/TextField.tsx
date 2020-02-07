1f30cedc807fc7116916c7171b2be5ca90eb57de
Eric Moynihan <git@moynihan.io>
2020-02-07 14:43:55 +0000

    Added onChangeText to TextField

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -8,24 +8,27 @@ export interface InputConfig extends Base {
   value?: string;
   subtitle?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangeText?: (_s?: string) => string;
 }
 export default function Input(props: InputConfig): JSX.Element {
-  const [value, setValue] = React.useState(props.value ?? "");
+  const [value, setValue] = React.useState(props.value ?? ""),
+    onChangeText = props.onChangeText;
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.currentTarget.value !== value) setValue(e.currentTarget.value);
+    if (e.currentTarget.value !== value) {
+      setValue(e.currentTarget.value);
+
+      if (onChangeText) onChangeText(e.currentTarget.value);
+    }
     if (props.onChange) props.onChange(e);
   };
+
   return (
     <div className={style.root}>
       {(() =>
         props.subtitle ? (
           <span className={style.sub}>{props.subtitle}</span>
         ) : null)()}
-      <input
-        onChange={onChange}
-        value={props.value}
-        style={props.style}
-      ></input>
+      <input onChange={onChange} value={value} style={props.style}></input>
     </div>
   );
 }
