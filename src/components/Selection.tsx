632299cfd34554649858db7154dde54c273ab404
Eric Moynihan <git@moynihan.io>
2020-02-11 15:13:05 +0000

    Added check state

--- src/components/Selection.tsx
+++ src/components/Selection.tsx
@@ -6,8 +6,12 @@ import * as style from "./styles/TextField.css";
 
 export interface SelectionConfig extends Base {
   type: "radio" | "checkbox" | "switch";
+  value?: string;
+  checked?: boolean;
 }
 export function Selection(props: SelectionConfig): JSX.Element {
+  const [checked, setChecked] = React.useState(props.checked),
+    ref = React.useRef<HTMLInputElement>();
   let type: string;
   if (props.type === "switch") {
     type = "radio";
@@ -17,7 +21,7 @@ export function Selection(props: SelectionConfig): JSX.Element {
 
   return (
     <div className={style.root}>
-      <input type={type} />
+      <input type={type} ref={ref} value={props.value} checked={checked} />
     </div>
   );
 }
