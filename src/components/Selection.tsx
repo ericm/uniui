1f3e94cc79a04d862115aa9819a9bb8238334ee7
Eric Moynihan <git@moynihan.io>
2020-02-15 18:40:24 +0000

    Adding Selection

--- src/components/Selection.tsx
+++ src/components/Selection.tsx
@@ -2,15 +2,18 @@ import * as React from "react";
 import { Base } from "./base";
 import { applyTheme, CTX } from "../theme";
 
-import * as style from "./styles/TextField.css";
+import * as style from "./styles/Selection.css";
 
 export interface SelectionConfig extends Base {
   type: "radio" | "checkbox" | "switch";
   value?: string;
   checked?: boolean;
+  label?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChangeValue?: (state: boolean) => void;
 }
 export function Selection(props: SelectionConfig): JSX.Element {
-  const [checked, setChecked] = React.useState(props.checked),
+  const [checked, setChecked] = React.useState(props.checked ?? false),
     ref = React.useRef<HTMLInputElement>();
   let type: string;
   if (props.type === "switch") {
@@ -19,9 +22,33 @@ export function Selection(props: SelectionConfig): JSX.Element {
     type = props.type;
   }
 
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.checked !== checked) {
+      setChecked(e.currentTarget.checked);
+      props.onChange(e);
+      props.onChangeValue(checked);
+    }
+  };
+
+  const render = () => {
+    switch (props.type) {
+    }
+  };
+
   return (
     <div className={style.root}>
-      <input type={type} ref={ref} value={props.value} checked={checked} />
+      <label>
+        {(() => (props.label ? <span>{props.label}</span> : null))()}
+        {render()}
+      </label>
+      <input
+        onChange={}
+        style={props.style && { display: "none" }}
+        type={type}
+        ref={ref}
+        value={props.value}
+        checked={checked}
+      />
     </div>
   );
 }
