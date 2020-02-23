3f13b09e5aee8cec6f49da4683782244c03990cb
Eric Moynihan <git@moynihan.io>
2020-02-23 18:13:18 +0000

    Webpack relase 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
+const nodeExternals = require('webpack-node-externals');
+
 module.exports = {
   entry: './src/index.ts',
   output: {
     path: __dirname + '/lib',
-    filename: 'index.js'
+    filename: 'index.js',
+    library: '',
+    libraryTarget: 'commonjs'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.css']
   },
+  externals: [nodeExternals()],
   module: {
     rules: [
       { test: /\.tsx?$/, loader: 'ts-loader' },
