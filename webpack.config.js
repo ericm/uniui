9d45a5472cf4b0ee4466e4b0996ab12e736153d5
Eric Moynihan <git@moynihan.io>
2020-02-26 14:31:21 +0000

    Release 1.1.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,18 @@
-const nodeExternals = require('webpack-node-externals');
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   output: {
-    path: __dirname + '/lib',
-    filename: 'index.js',
-    library: '',
-    libraryTarget: 'umd'
+    path: __dirname + "/lib",
+    filename: "index.js",
+    library: "",
+    libraryTarget: "umd"
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.css']
+    extensions: [".ts", ".tsx", ".js", ".css"]
   },
   externals: [nodeExternals()],
   module: {
-    rules: [
-      { test: /\.tsx?$/, loader: 'ts-loader' },
-      {
-        test: /\.css$/,
-        use: [{ loader: "css-loader", options: { modules: true } },
-          "postcss-loader"]
-      }
-    ]
+    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }]
   }
-}
+};
