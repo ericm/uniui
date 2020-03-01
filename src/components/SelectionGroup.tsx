6600eeca192b3abea27685cfa4d4094da3fdcd93
Eric Moynihan <git@moynihan.io>
2020-03-01 14:36:10 +0000

    Fixed Radio contect

--- src/components/SelectionGroup.tsx
+++ src/components/SelectionGroup.tsx
@@ -5,18 +5,21 @@ export interface SelectMap {
   onChange: (i: number) => void;
 }
 
-const map: SelectMap = {
-  changers: [],
-  onChange: (curr: number) => {
-    map.changers.forEach((handle, i) => {
-      if (i !== curr) handle();
-    });
+const newMap = (): SelectMap => {
+  const map = {
+    changers: [],
+    onChange: (curr: number) => {
+      map.changers.forEach((handle, i) => {
+        if (i !== curr) handle();
+      });
+    }
   }
+  return map
 };
-const ctx = React.createContext(map);
+const ctx = React.createContext(newMap());
 
-export default function(props: Base) {
-  return <ctx.Provider value={map}>{props.children}</ctx.Provider>;
+export default function (props: Base) {
+  return <ctx.Provider value={newMap()}>{props.children}</ctx.Provider>;
 }
 
 export { ctx as SelectionContext };
