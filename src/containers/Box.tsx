215625c19d44f4673ed0dd0143a17c77ade430e4
Eric Moynihan <git@moynihan.io>
2020-03-04 14:55:53 +0000

    Implementing Box

--- src/containers/Box.tsx
+++ src/containers/Box.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
-
+import { cloneDeep } from "lodash";
 import styled from "styled-components";
 
-export interface BoxConfig {}
+export interface BoxConfig {
+  children: Array<React.ReactElement | JSX.Element>;
+  layout: "horizontal" | "vertical";
+}
 function Box(props: BoxConfig): JSX.Element {
-  return <div></div>;
+  let children = cloneDeep(props.children);
+  for (let child of children) {
+  }
+  return <div>{children}</div>;
 }
 export default styled(Box);
