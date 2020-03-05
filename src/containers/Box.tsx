4fdf4dd34f706c6da99b33abbd0452feaa8b286a
Eric Moynihan <git@moynihan.io>
2020-03-05 11:32:28 +0000

    Added Box layout

--- src/containers/Box.tsx
+++ src/containers/Box.tsx
@@ -3,12 +3,18 @@ import { cloneDeep } from "lodash";
 import styled from "styled-components";
 import { Base } from "../components/base";
 
-const Root = styled.div`
-  flex-wrap: wrap;
-  justify-content: space-around;
-  align-items: center;
-  flex: 0 0 100%;
-  display: flex;
+const Root = styled.div<{ layout: "horizontal" | "vertical" }>`
+  display: grid;
+  padding: 2em;
+  grid-gap: 3em;
+  ${({ layout }) =>
+    (layout === "horizontal" &&
+      `
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  `) ||
+    (layout === "vertical" &&
+      `
+  `)}
 `;
 export interface BoxConfig {
   children: Array<React.ReactElement<Base>>;
@@ -18,11 +24,8 @@ export default function Box(props: BoxConfig): JSX.Element {
   let children = cloneDeep(props.children);
   switch (props.layout) {
     case "horizontal":
-      for (let child of children) {
-        child.props.style = { padding: "2em" };
-      }
       break;
   }
-  return <Root>{children}</Root>;
+  return <Root layout={props.layout}>{children}</Root>;
 }
 export const BoxStyled = styled(Box);
