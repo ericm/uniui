225d304c366776cf8176e1bce4ebd436aecf08f9
Eric Moynihan <git@moynihan.io>
2020-03-15 15:27:52 +0000

    Theming docz map

--- docs/theme/index.jsx
+++ docs/theme/index.jsx
@@ -1,3 +1,27 @@
 import * as React from 'react';
 import { theme, useConfig, ComponentsProvider } from 'docz';
-import { ThemeProvider } from 'theme-ui'
+import { ThemeProvider } from 'theme-ui';
+
+import { Menu } from "./Menu";
+
+const Theme = ({ children }) => {
+  const config = useConfig()
+  return (
+    <ThemeProvider theme={config}>
+      <Menu />
+      <ComponentsProvider components={map}>
+        {children}
+      </ComponentsProvider>
+    </ThemeProvider>
+  )
+}
+
+const themeConfig = {
+  colors: {
+    primary: 'tomato',
+    secondary: 'khaki',
+    gray: 'lightslategray',
+  },
+}
+export default theme(themeConfig)(Theme)
+
