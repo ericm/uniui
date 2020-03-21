7aa7ad2c5eb1394b75bddfa491c32bd05da9ef03
Eric Moynihan <git@moynihan.io>
2020-03-21 17:06:52 +0000

    Fixed copy of Base Attributes

--- src/components/base.ts
+++ src/components/base.ts
@@ -2,11 +2,6 @@ import * as React from "react";
 
 export function getBaseAttributes<E extends Element, T extends Base<E>>(obj: T): React.HTMLAttributes<E> {
   let newObj: React.HTMLAttributes<E> = Object.create(obj);
-  for (let key of Object.keys(obj)) {
-    if (key in newObj) {
-      newObj[key] = obj[key]
-    }
-  }
   return newObj;
 }
 
